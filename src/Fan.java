cfddc6a6a3db9d65a6fe7ed15c999120f2d84338
Ankith Talpady <ankithtalpady05@gmail.com>
2024-11-29 03:35:45 -0500

    Updated

--- src/Fan.java
+++ src/Fan.java
@@ -57,7 +57,7 @@ public class Fan {
         fan1.setSpeed(Fan.FAST);
         fan1.setRadius(10);
         fan1.setColor("Yellow");
-        fan1.setFanOn(true); // Fixed: should use setFanOn instead of setFanOnOff
+        fan1.setFanOn(true);
 
         System.out.println(fan1);
 
@@ -65,7 +65,7 @@ public class Fan {
         fan2.setSpeed(Fan.MEDIUM);
         fan2.setRadius(5);
         fan2.setColor("Blue");
-        fan2.setFanOn(false); // Fixed: should use setFanOn instead of setFanOnOff
+        fan2.setFanOn(false);
 
         System.out.println(fan2);
     }
